df338d4b1ef7c66a0c75892582d3871618617fcb
root <justinjams@gmail.com>
2015-03-22 23:15:09 -0400

    Fix device variant config files to match source tree

--- cm.mk
+++ cm.mk
@@ -2,8 +2,7 @@
 PRODUCT_RELEASE_NAME := GN-GSM
 
 # Boot animation
-# TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
@@ -18,9 +17,9 @@ $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
 PRODUCT_NAME := cm_maguro
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
+# PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
